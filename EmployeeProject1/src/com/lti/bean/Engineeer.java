e7a42a9092ccfaebf4bb9478ad6835b7171617bd
BitadruMegha <bitadrumaitrabm@gmail.com>
2019-07-25 09:55:02 -0700

    employee 1 got changed

--- EmployeeProject1/src/com/lti/bean/Engineeer.java
+++ EmployeeProject1/src/com/lti/bean/Engineeer.java
@@ -20,6 +20,13 @@ public class Engineeer extends Employee {
 	public void setProject(String project) {
 		this.project = project;
 	}
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("added value is"+c );
+	}
 	
 	
 
